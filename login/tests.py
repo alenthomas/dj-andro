35d842113bfcc4d1469c57a3cb3215dc5ce1f83d
alenthomas <alenthomas9@gmail.com>
2016-01-29 12:55:08 +0530

    checks for the register web-page rendering

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+class LoginTest(TestCase):
+    def test_reg_form(self):
+        """
+        Test for proper rendering of register form
+        """
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("form", response.context)
