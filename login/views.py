0ebe0e871829aad268d4f36d821cd663ea86a16b
alenthomas <alenthomas9@gmail.com>
2016-01-30 03:22:38 +0530

    adds views for user registration

--- login/views.py
+++ login/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
+from .forms import RegisterForm
 # Create your views here.
 def register(request):
-    return HttpResponse("Hello world")
+    if request.method == "POST":
+        new_user = RegisterForm(request.POST)
+        if new_user.is_valid():
+            new_user.save()
+            return HttpResponseRedirect(reverse('success'))
+            
+    else:
+        form = RegisterForm()
+    return render(request, 'login/register.html',
+                  {'form':form,})
 
 # login
 
