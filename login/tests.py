bb7a75c83de72b0f3a3f675c38d579359533c38c
alenthomas <alenthomas9@gmail.com>
2016-01-29 13:03:44 +0530

    Changes Class name to RegisterTest

--- login/tests.py
+++ login/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 # Create your tests here.
-class LoginTest(TestCase):
+class RegisterTest(TestCase):
     def test_reg_form(self):
         """
         Test for proper rendering of register form
