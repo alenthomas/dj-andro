a6df9ab91c0a2a59ae9957af5d24f89076ce0d61
alenthomas <alenthomas9@gmail.com>
2016-01-30 02:55:11 +0530

    adds user and comment models

--- login/models.py
+++ login/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.username
+
+class Comments(models.Model):
+    username = models.ForeignKey(Users)
+    title = models.CharField(max_length=25)
+    message = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.title
