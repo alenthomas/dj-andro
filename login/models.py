ccb24d04b67edca1cddeffa5ba1a8690de99509a
alenthomas <alenthomas9@gmail.com>
2016-01-30 15:28:52 +0530

    refactors comment to about

--- login/models.py
+++ login/models.py
@@ -9,10 +9,9 @@ class Login(models.Model):
     def __str__(self):
         return self.email_id
 
-class Comments(models.Model):
-    username = models.ForeignKey(Users)
-    title = models.CharField(max_length=25)
-    message = models.TextField(max_length=100)
+class About(models.Model):
+
+    about = models.TextField(max_length=100)
 
     def __str__(self):
-        return self.title
+        return self.id
