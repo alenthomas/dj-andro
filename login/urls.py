f51246d113f14ebab7260185e9ceb0d713ea85bf
alenthomas <alenthomas9@gmail.com>
2016-01-30 11:09:19 +0530

    adds urls with proper trailing slashes and $

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'login'
 urlpatterns = [
-    url(r'^register', views.register, name='register'),
-    url(r'^success/', views.success, name='success'),
-    url(r'^error/', views.error, name='error'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^success/$', views.success, name='success'),
+    url(r'^error/$', views.error, name='error'),
+    url(r'^successlogin/$', views.login_success, name='loginok'),
+    url(r'^errorlogin/$', views.login_error, name='loginerror'),
     ]
