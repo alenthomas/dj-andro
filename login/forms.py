9a93fb7045cf859ea83dce2029b33d1d877a3eb2
alenthomas <alenthomas9@gmail.com>
2016-01-30 13:22:32 +0530

    adds comment-model form

--- login/forms.py
+++ login/forms.py
@@ -9,3 +9,14 @@ class RegisterForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=10)
     password = forms.CharField(max_length=10)
+
+class AddCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = {'username', 'title', 'message'}
+        """
+        reorder the form fields in the model form
+        """
+    def __init__(self, *args, **kwargs):
+        super(AddCommentForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = ['username', 'title', 'message']
