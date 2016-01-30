69da074af574497cca5304ce22c21bf650ebfd98
alenthomas <alenthomas9@gmail.com>
2016-01-30 12:01:00 +0530

    adds models to admin site

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Users
+from .models import Comments
+
+admin.site.register(Users)
+admin.site.register(Comments)
