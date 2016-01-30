3b5050ea90d6860487af4539d927ccf9e8cc2745
alenthomas <alenthomas9@gmail.com>
2016-01-30 11:08:48 +0530

    modifies form to make it a simple form

--- login/forms.py
+++ login/forms.py
@@ -5,3 +5,7 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = {'username', 'password'}
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=10)
+    password = forms.CharField(max_length=10)
