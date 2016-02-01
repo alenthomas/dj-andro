4bb1f1ed894ff037aba34a02e7c75b13e3065a4a
alenthomas <alenthomas9@gmail.com>
2016-02-01 10:53:44 +0530

    makes email and mobile_no field unique and also edits return statement to return string

--- login/models.py
+++ login/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Login(models.Model):
-    email_id = models.EmailField(max_length=40)
+    email_id = models.EmailField(max_length=40, unique=True)
     password = models.CharField(max_length=35)
-    mobile_number = models.CharField(max_length=13)
+    mobile_number = models.CharField(max_length=13, unique=True)
     hint = models.CharField(max_length=40)
 
     def __str__(self):
@@ -14,4 +14,4 @@ class About(models.Model):
     about = models.TextField(max_length=100)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
